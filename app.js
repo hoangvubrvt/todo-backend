cc052a838a2d6cd1bef2c039a6d14d4d828c49ea
Vu Hoang <hoangvubrvt@gmail.com>
2017-02-22 05:37:21 +0700

    Fixed error for front-end

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ module.exports = require('./database').connect(() => {
         server: serverConfig
     } = require('./config');
     var app = express();
+    app.all('/*', function(req, res, next){
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+        next();
+    });
     app.use(bodyParser.urlencoded({
         extended: true
     }));
